a7c13189b5ca8d3af5c94d3b5e6bdfdf828947b0
GoDaeGyeon <dae.gyeon.go@gmail.com>
2025-03-25 12:46:58 +0900

    test

--- backend/api/crud-lead.js
+++ backend/api/crud-lead.js
@@ -1,10 +1,6 @@
-// const { MongoClient } = require("mongodb");
-import { request } from "express";
 import { MongoClient } from "mongodb";
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb+srv://demo_user:0717@cluster0.mfakl.mongodb.net/";
+const uri = MONGODB_ATLAS_URI;
 const client = new MongoClient(uri);
-// const dbName = ''
 export async function readEmployees() {
   try {
     await client.connect();
@@ -16,11 +12,7 @@ export async function readEmployees() {
 
   } catch (e) {
     console.log(e);
-/*   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close(); */
+  } finally {
+    await client.close();
   }
 }
-readEmployees().catch(console.dir);
-
-// export default run
--- backend/api/index.js
+++ backend/api/index.js
@@ -1,88 +1,18 @@
-// const express = require('express')
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-// import { GET } from ".api/index.js";
-import { readEmployees } from "./crud-lead.js";
-import { createEmployees } from "./crud-create.js";
-import { deleteEmployees } from "./crud-delete.js";
-import { updateEmployees } from "./crud-update.js";
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.get("/employees", async (req, res) => {
+import { MongoClient } from "mongodb";
+const uri = MONGODB_ATLAS_URI;
+const client = new MongoClient(uri);
+export async function GET(request) {
   try {
-    const employees = await readEmployees();
-    const { name, email } = employees;
-    res.status(200).json(employees);
+    await client.connect();
+    const db = client.db("hr");
+    const coll = db.collection("employees");
+    const cursor = coll.find();
+    const users = await cursor.toArray()
+    return users;
+
   } catch (e) {
     console.log(e);
-    res.status(500).send("DB 연결 오류가 발생했슴니다");
+  } finally {
+    await client.close();
   }
-});
-app.post("/employees", async (req, res) => {
-  // console.log(req.body);
-  // res.send("Create movie");
-  try {
-    const result = await createEmployees(req.body);
-    res.status(201).json({
-      status: "success",
-      data: result,
-    });
-  } catch (e) {
-    console.log("데이터 등록중 애러 : ", e);
-    res.status(500).json({
-      status: "fail",
-      message: "데이터 등록 실패",
-    });
-  }
-});
-app.put("/employees/:id", async (req, res) => {
-  console.log(target);
-
-  try {
-    const request = await updateUser({
-      id: req.params.id,
-      data: req.body,
-    });
-    res.status(201).json(result);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      status: "fail",
-      message: "데이터 업데이트 실패",
-    });
-  }
-});
-app.delete("/employees/:id", async (req, res) => {
-  try {
-    const result = await deleteEmployees({ _id: req.params.id });
-    res.status(201).json({
-      status: "success",
-      data: result,
-      message: "사원정보가 삭제되었습니다",
-    });
-  } catch (e) {
-    console.log("데이터 등록중 애러 : ", e);
-    res.status(500).json({
-      status: "fail",
-      message: "삭제요청실패",
-    });
-  }
-});
-console.log(process.env.NODE_ENV);
-
-if (process.env.NODE_ENV === "production") {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-    readEmployees();
-  });
 }
