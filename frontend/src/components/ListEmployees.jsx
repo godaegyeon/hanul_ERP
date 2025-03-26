7d9dad207b3a27e040723b5007bac5905752f4f8
GoDaeGyeon <dae.gyeon.go@gmail.com>
2025-03-26 12:17:43 +0900

    frontend 경로 변경

--- frontend/src/components/ListEmployees.jsx
+++ frontend/src/components/ListEmployees.jsx
@@ -10,7 +10,7 @@ function ListEmployees() {
   const [isModalShow, setIsModalShow] = useState(false);
   const [editEmployee, setEditEmployee] = useState(null);
   const getEmployees = async () => {
-    const data = await axios.get('https://hanul-erp-backend-xi.vercel.app/employees');
+    const data = await axios.get('/employees');
     // fetch() : json parse 직접 ==> response.json() 호출
     // axios() : json parse 직접 ==> state에 담의면 됨
     setEmployees(data.data);
