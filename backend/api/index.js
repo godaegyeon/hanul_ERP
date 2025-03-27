fb028e09674ee1a9604f7a76b371406171496d92
GoDaeGyeon <dae.gyeon.go@gmail.com>
2025-03-27 10:09:33 +0900

    update coll -> employees 명칭 변경

--- backend/api/index.js
+++ backend/api/index.js
@@ -17,21 +17,21 @@ export default async function handler(req, res) {
   const client = new MongoClient(process.env.MONGODB_URI);
   await client.connect();
   const db = client.db("hr");
-  const coll = db.collection("employees");
+  const employees = db.collection("employees");
 
   try {
     switch (req.method) {
       case "GET":
-        const allEmployees = await coll.find().toArray();
+        const allEmployees = await employees.find().toArray();
         res.status(200).json(allEmployees);
         break;
       case "POST":
         const newEmployee = req.body;
-        const result = await coll.insertOne(newEmployee);
+        const result = await employees.insertOne(newEmployee);
         res.status(200).json(result);
         break;
       default:
-        res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+        res.setHeader("Allow", ["GET", "POST"]);
         res.status(405).end(`${req.method}는 허용되지 않습니다`);
     }
   } catch (e) {
